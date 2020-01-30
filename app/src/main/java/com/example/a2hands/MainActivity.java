ca171a6fb3c208a819c5171c4f71a30e30f5edfd
abdallahmagdy15 <abdallah.magdy1515@gmail.com>
2020-01-30 14:39:43 +0200

    try branch task 4

--- app/src/main/java/com/example/a2hands/MainActivity.java
+++ app/src/main/java/com/example/a2hands/MainActivity.java
@@ -11,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String selectedCat = "general";
+        String country="Egypt";
+        String region = "KFS";
+        
+
 
     }
 }
