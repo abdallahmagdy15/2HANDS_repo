42b5fe487ffbfae6b73eb212aa7674a8b19467f7
abdallahmagdy15 <abdallah.magdy1515@gmail.com>
2020-01-31 04:22:05 +0200

    firebase installed

--- app/src/main/java/com/example/a2hands/MainActivity.java
+++ app/src/main/java/com/example/a2hands/MainActivity.java
@@ -1,15 +1,56 @@
 package com.example.a2hands;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Parcel;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.google.android.gms.internal.firebase_auth.zzcz;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class MainActivity extends AppCompatActivity {
+    public FirebaseAuth mAuth;
+    public FirebaseUser fu;
+    public DatabaseReference dbRef;
 
+    String email;
+    String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
 }
