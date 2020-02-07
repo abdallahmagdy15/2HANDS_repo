bf8561e1f3f386ba9cacbb2e93f40fe4b8cee095
abdallahmagdy15 <abdallah.magdy1515@gmail.com>
2020-02-07 19:02:58 +0200

    from start to home activities are created as empty

---
+++ app/src/main/java/com/example/a2hands/signUpSetFavTopicsActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2hands;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signUpSetFavTopicsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_set_fav_topics);
+    }
+}
---
+++ app/src/main/java/com/example/a2hands/signupCreatePasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.a2hands;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signupCreatePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_create_password);
+    }
+}
