23dda83318cdf03d798b3e3a64115d5c7cd7b12d
Waleed <waleedyounis1541998@gmail.com>
2020-02-21 06:02:08 +0200

    Camera PickImage Added and Upload it to firestore

--- app/src/main/java/com/example/a2hands/CreatePost.java
+++ app/src/main/java/com/example/a2hands/CreatePost.java
@@ -3,12 +3,21 @@ package com.example.a2hands;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -36,6 +45,9 @@ import com.google.firebase.storage.StorageTask;
 import com.squareup.picasso.Picasso;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 
 
@@ -56,9 +68,18 @@ public class CreatePost extends AppCompatActivity {
     StorageTask uploadTask;
     StorageReference storageReference;
     private static final int PICK_IMAGE_REQUEST = 1;
+
     ImageView selectedImage;
     Button add_image;
     Post post;
+
+    //cameraRequest
+    //camera request
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    Button camerabtn;
+    String currentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,7 +93,15 @@ public class CreatePost extends AppCompatActivity {
         createdPostIsAnon = findViewById(R.id.createdPostIsAnon);
         db = FirebaseDatabase.getInstance().getReference();
         ownerPic = findViewById(R.id.postOwnerPic);
+        camerabtn = findViewById(R.id.camerabtn);
+        camerabtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askCameraPermissions();
+            }
+        });
 
+        //camerabtn
         //addimage decleartion
         storageReference = FirebaseStorage.getInstance().getReference("posts");
         selectedImage = findViewById(R.id.selectedImage);
@@ -125,6 +154,11 @@ public class CreatePost extends AppCompatActivity {
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
+    private String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
     public void submitPost() {
         post = new Post();
         post.category = catSpinner.getSelectedItem().toString();
@@ -137,7 +171,7 @@ public class CreatePost extends AppCompatActivity {
         post.user_id = uid;
         post.profile_pic = user.profile_pic;
         if (imageUri != null){
-            final StorageReference fileReference = storageReference.child(System.currentTimeMillis() + ".jpg");
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
             uploadTask = fileReference.putFile(imageUri);
             uploadTask.continueWithTask(new Continuation() {
                 @Override
@@ -191,9 +225,6 @@ public class CreatePost extends AppCompatActivity {
             startActivity(new Intent(CreatePost.this, MainActivity.class));
             finish();
         }
-
-
-
         /*db.child("users").child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -239,9 +270,84 @@ public class CreatePost extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK){
+        if(requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK){
+            File f = new File(currentPhotoPath);
+            imageUri = Uri.fromFile(f);
+            selectedImage.setImageURI(imageUri);
+
+        }
+        else if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK){
             imageUri = data.getData();
             selectedImage.setImageURI(imageUri);
+        }
+        else {
+            startActivity(new Intent(CreatePost.this, MainActivity.class));
+            finish();
+        }
+    }
+
+
+    //methods to convert bitmap to uriImage to upload it
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File...
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.a2hands.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+
+    //camera methods
+    private void askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        }else {
+            dispatchTakePictureIntent();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == CAMERA_PERM_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                dispatchTakePictureIntent();
+            }else {
+                Toast.makeText(this, "Camera Permission is Required to Use camera.", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
